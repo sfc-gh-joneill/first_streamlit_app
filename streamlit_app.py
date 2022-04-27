1f6e38e8d1e207402aee502e738d7519b9145bb1
Jim Oneill <97119519+sfc-gh-joneill@users.noreply.github.com>
2022-04-27 11:02:07 -0400

    typos or gtfo

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selectes]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on page 
 streamlit.dataframe(fruits_to_show)
 
