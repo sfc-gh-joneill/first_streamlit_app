8e5ea38b9f670725f6b75aeb94083e71ae853934
Jim Oneill <97119519+sfc-gh-joneill@users.noreply.github.com>
2022-04-27 10:50:04 -0400

    remove first dataframe. add index to my_fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # We'll add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies.
 streamlit.multiselect("Pick some fruits", list(my_fruit_list.index))
